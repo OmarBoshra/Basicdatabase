7e0c82b0b128ae03666c31d4fc232e55538235c2
Omar Boshra <47230931+OmarBoshra@users.noreply.github.com>
2019-12-20 05:32:53 +0200

    Update retrive.php

--- retrive.php
+++ retrive.php
@@ -1,24 +1,14 @@
 <?php
-
 require "connection.php";
-
 $user_name=$_POST["name"];
 $user_age=$_POST["age"];
-
 $sql_query="select name from names where name like '$user_name' and age like '$user_age';";
-
 $result=mysqli_query($connect,$sql_query);
-
 if(mysqli_num_rows($result)>0){
-
 $firstrow = mysqli_fetch_assoc($result);
 $name = $firstrow["name"];
-
-echo "Hello Welcome".$name;
-
+echo "Hello Welcome ".$name;
 }else{
 echo "no info available";
 }
-
-
 ?>
