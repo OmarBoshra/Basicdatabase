44e2e0e7652c7251d48be2442f3a94a7c2d0d1d8
Omar Boshra <47230931+OmarBoshra@users.noreply.github.com>
2019-12-20 05:33:35 +0200

    Update update.php

--- update.php
+++ update.php
@@ -9,7 +9,7 @@ $user_age = $_POST["nage"];
 $sql_query = "update names set name ='$user_name' , age = '$user_age' where name ='$olduser_name';";
 
 if(mysqli_query($connect,$sql_query)){
-echo "update sucess";
+echo "update success";
 }else{
 echo "no update";
 }
